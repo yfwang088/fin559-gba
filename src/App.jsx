5d4616da9dba6638a360db34bfcd4823699034a3
yfwang088 <157369864+yfwang088@users.noreply.github.com>
2024-03-06 05:45:48 +0800

    React base

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DirectionsBusOutlined,MyLocationOutlined, PlaceOutlined, DepartureBoardOutlined, PlayArrow } from "@mui/icons-material";
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import { Container, CssBaseline, Button,Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const App = () => {
+  return <div>hello</div>;
+};
+
+export default App;
