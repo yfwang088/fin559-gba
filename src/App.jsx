12a0221e4738e1a307dc1ed46da796dafaea002e
yfwang088 <157369864+yfwang088@users.noreply.github.com>
2024-03-06 19:49:53 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/Add';
 import Box from '@mui/material/Box';
