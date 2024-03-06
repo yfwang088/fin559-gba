11c60f3da952e65aff563283fd66d950461d1f00
yfwang088 <157369864+yfwang088@users.noreply.github.com>
2024-03-06 17:39:56 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/Add';
-import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
 import { Container, CssBaseline, Button,Typography } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
@@ -16,8 +17,6 @@ const client = new DynamoDBClient({
   },
   });
 const docClient = DynamoDBDocumentClient.from(client);
-
-const testman = {id:"12345", name:"Bay Stonefield", age:"35", pos:"Developer"};
 
 const getEmployeeByID = (id) => new Promise(async (resolve, reject) => {
   try {
@@ -55,6 +54,32 @@ const putEmployee = (id, name, age, position) => new Promise(async (resolve, rej
 });
 
 
+const InputField = ({id, label, value, setfucn}) => {
+
+  const handleInputChange = (event) => {
+    setfucn(event.target.value);
+  };
+
+  return (
+      <TextField
+          required
+          id={id}
+          label={label}
+          value={value}
+          onChange={handleInputChange}
+          variant="filled"
+        />
+  );
+};
+
+const TextPanel = ({ text }) => {
+  return (
+    <Paper style={{ width: '400px', height: '200px', padding: '16px', overflow: "auto",}}>
+      <Typography variant="body1">{text}</Typography>
+    </Paper>
+  );
+};
+
 
 const App = () => {
   const theme = createTheme({
@@ -65,24 +90,42 @@ const App = () => {
 
   const [b1Running, setB1Running] = useState(false);
   const [b2Running, setB2Running] = useState(false);
+  const [IDText, setIDText] = useState("");
+  const [nameText, setNameText] = useState("");
+  const [ageText, setAgeText] = useState("");
+  const [posText, setPosText] = useState("");
+  const [searchText, setSearchText] = useState('');
+  const [resultText, setResultText] = useState('The result will show here.');
+
 
   const b1Click = async () => {
     setB1Running(true);
-    await getEmployeeByID(testman.id).then((data) =>{
-      console.log(data.Item);
-    }).catch((ex)=>{
-      console.log(ex);
+    await putEmployee(IDText, nameText,ageText,posText).then(setResultText('Success! A new employee has been recorded.')).catch((ex)=>{
+      setResultText(ex.message);
     });
+    setResultText('Success!')
     setB1Running(false);
   };
   
   const b2Click = async () => {
     setB2Running(true);
-    await putEmployee(testman.id, testman.name,testman.age,testman.pos).catch((ex)=>{
+    await getEmployeeByID(searchText).then((data) =>{
+      if(data.Item){
+        setResultText(`${data.Item.ID},${data.Item.Name},${data.Item.Age},${data.Item.Position}`);
+      }else{
+        setResultText("ID does not match existing record.");
+      }
+      console.log(data.Item);
+    }).catch((ex)=>{
       console.log(ex);
     });
     setB2Running(false);
   };
+
+  const isEmpty = (value) =>{
+    return value === "";
+  };
+
 
 
   return (
@@ -102,26 +145,88 @@ const App = () => {
         <Typography id='title' variant='h3'>
             Employee Management System
         </Typography>
-        <nav>
+          <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+              <div>
+        <InputField
+          id="add-id"
+          label="ID"
+          value={IDText}
+          setfucn={setIDText}
+        />
+        <InputField
+          id="add-name"
+          label="Name"
+          value={nameText}
+          setfucn={setNameText}
+        />
+        <InputField
+          id="add-age"
+          label="Age"
+          value={ageText}
+          setfucn={setAgeText}
+        />
+        <InputField
+          id="add-pos"
+          label="Position"
+          value={posText}
+          setfucn={setPosText}
+        />
         <Button
           variant='outlined'
           size='large'
-          startIcon={<AddIcon />}
+          startIcon={<AddIcon/>}
           onClick={b1Click}
-          disabled={b1Running||b2Running}
+          disabled={b1Running||b2Running||isEmpty(IDText)||isEmpty(nameText)||isEmpty(ageText)||isEmpty(posText)}
         >
-        Search
+        Add
         </Button>
+      </div>
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+        <InputField
+          id="serach-id"
+          label="Search by ID"
+          value={searchText}
+          setfucn={setSearchText}
+        />
         <Button
           variant='outlined'
           size='large'
           startIcon={<SearchIcon />}
           onClick={b2Click}
-          disabled={b1Running||b2Running}
+          disabled={b1Running||b2Running||isEmpty(searchText)}
         >
-        Add
+        Search
         </Button>
-      </nav>
+        </div>
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '30ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+      <div>
+      <TextPanel text={resultText} />
+      </div>
+      </Box>
         </Container>
         </ThemeProvider>
         </>
